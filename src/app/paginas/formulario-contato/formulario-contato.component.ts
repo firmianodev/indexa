60243bdd873222a62b7222f32dd852daf95f1705
firmianodev <firmiano.dev@gmail.com>
2024-09-23 10:20:17 -0300

    separando as telas e criando as rotas

--- src/app/paginas/formulario-contato/formulario-contato.component.ts
+++ src/app/paginas/formulario-contato/formulario-contato.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { ContainerComponent } from '../../componentes/container/container.component';
 import { SeparadorComponent } from "../../componentes/separador/separador.component";
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-formulario-contato',
@@ -9,7 +10,8 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
   imports: [
     ContainerComponent,
     SeparadorComponent,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    CommonModule
 ],
   templateUrl: './formulario-contato.component.html',
   styleUrl: './formulario-contato.component.css'
@@ -20,12 +22,22 @@ export class FormularioContatoComponent {
 
   constructor() {
     this.contatoForm = new FormGroup({
-      nome: new FormControl(''),
-      telefone: new FormControl(''),
-      email: new FormControl(''),
+      nome: new FormControl('', Validators.required),
+      telefone: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
       aniversario: new FormControl(''),
       redes: new FormControl(''),
       observacoes: new FormControl('')
     })
+  }
+
+  salvarContato() {
+    if(this.contatoForm.valid) {
+      console.log(this.contatoForm.value)
+    } 
+  }
+
+  cancelar() {
+    console.log('Submissao cancelada')
   }
 }
